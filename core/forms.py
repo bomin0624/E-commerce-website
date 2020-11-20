4934a773798e89bc1ce838c4a2d678e2af577895
bevis00 <bevis5555555@gmail.com>
2020-11-20 19:52:37 +0900

    checkout forms layout modified 1:57:26

--- core/forms.py
+++ core/forms.py
@@ -27,8 +27,8 @@ class CheckoutForm(forms.Form):
         'class': 'form-control',
         'id': 'zip'
     }))
-    same_billing_address = forms.BooleanField(widget=forms.CheckboxInput())
-    same_shipping_address = forms.BooleanField(required=False)
+    same_billing_address = forms.BooleanField(required=False)
+    #same_shipping_address = forms.BooleanField(required=False)
     save_info = forms.BooleanField(required=False)
     payment_option = forms.ChoiceField(
         widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
